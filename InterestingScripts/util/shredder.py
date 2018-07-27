358ba4819e429d9c24834f52a5b1785cfa0d4f4a
HLOverflow <snowyx10mini@gmail.com>
2018-07-28 04:47:34 +0800

    added progress bar, converted into usable module

--- InterestingScripts/util/shredder.py
+++ InterestingScripts/util/shredder.py
@@ -15,47 +15,55 @@
 #
 ##########################
 
-import tkFileDialog
-import Tkinter
-import os
-import random
+# version 2. 
+import random, os
+import sys
 
-# variables
-PASSES = 3
-
-# get file path
-root = Tkinter.Tk()
-root.withdraw()
-filepath = tkFileDialog.askopenfilename()
-print filepath
-root.destroy()
-
-# get size
-length = os.path.getsize(filepath)
-print length
+def printprogress(total, progress):
+    chars = ["*", "|", "/", "-", "\\", "|", "/", "-", "\\"]
+    progressbar = (progress / (total/20))*'#'
+    sys.stdout.write("\r[%s] [%-20s] %3d %%" % (chars[printprogress.index],progressbar, 100*progress/total) )
+    sys.stdout.flush()
+    printprogress.index = (printprogress.index + 1) % len(chars)
+printprogress.index=0
 
 # generate random strings to replace
 def getReplacement(length):
     replacement = ''
+    progress = 0
     for i in range(length):
         index = random.randint(33, 254)
         replacement += chr(index)
+        progress += 1
+        printprogress(length, progress)
+    print
     return replacement
 
-# overwrite file
-fo = open(filepath, "w")
-for count in range(PASSES):
-    data = getReplacement(length)
-    fo.write(data)
-    fo.seek(0)
-fo.close()
+def shred(filepath, passes=3):
+    # get size
+    length = os.path.getsize(filepath)
+    print length
 
-os.rename(filepath, "deadfile")
-os.unlink("deadfile")
-print "file has been shredded"
+    # overwrite file
+    fo = open(filepath, "w")
+    for count in range(passes):
+        print "pass number: ", count
+        data = getReplacement(length)
+        fo.write(data)
+        fo.seek(0)
+        print "pass", count, "finished"
+    fo.close()
 
+    os.rename(filepath, "deadfile")
+    os.unlink("deadfile")
+    print "file has been shredded"
 
-
-
-
-
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "usage:", argv[0], "<filepath>", "[numofpass=3]"
+        exit(0)
+    if len(sys.argv) == 2:
+        shred(sys.argv[1])
+    else:
+        shred(sys.argv[1], int(sys.argv[2]))
+    print "[*] done"
